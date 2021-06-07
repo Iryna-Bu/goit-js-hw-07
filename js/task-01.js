c8e6d588d0dfd5e0898b32ef0f493f66df52a339
Iryna-Bu <burgelo@gmail.com>
2021-06-07 19:51:35 +0300

    initial

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const NavEl = document.querySelectorAll('.item');
+console.log(NavEl);
+const NavTitle = document.getElementsByTagName('h2');
+console.log(NavTitle);
+const NavCategories = document.getElementById('categories');
+console.log(NavCategories);
---
+++ js/task-02.js
@@ -0,0 +1,13 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const element = document.createElement('li');
+  element.classList.add('item');
+  element.textContent = ingredients $();
+  console.log(element);
